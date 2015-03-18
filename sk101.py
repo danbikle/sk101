adbf279c1924ac5893c11a18a1bd85c07cd0ddbd
Dan Bikle <dan.bikle@gmail.com>
2015-03-18 23:02:43 +0000

    enhancements

--- sk101.py
+++ sk101.py
@@ -18,7 +18,8 @@ obs_count = len(df1)
 print('I have this many observations: '+str(obs_count))
 
 # I should get this many predictions:
-pcount = 10
+pcount = 2390 # Near the max
+pcount = 30
 
 # I should learn from this many observations:
 train_count = 252 * 10
