d090904351a273837c283f67d2fcedfeff67dbf0
Dan Bikle <dan.bikle@gmail.com>
2015-03-18 21:06:20 +0000

    enhancements

--- sk101.bash
+++ sk101.bash
@@ -6,6 +6,16 @@
 # Ref:
 # http://finance.yahoo.com/q?s=^GSPC
 
+if [ -e ~ann/sk101/ ]; then
+  echo $0 is in the correct folder.
+else
+  echo $0 needs to reside here:
+  echo ~ann/sk101/
+  echo bye.
+  exit 1
+fi
+
+mkdir -p /tmp/sk101/
+
 
 exit
-
