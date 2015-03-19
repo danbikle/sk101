7f7baaf442b546186f43f91c35ca913589679295
Dan Bikle <dan.bikle@gmail.com>
2015-03-19 02:32:26 +0000

    enhancements

--- plotem.bash
+++ plotem.bash
@@ -7,4 +7,12 @@
 # Demo:
 # /home/ann/sk101/plotem.bash /tmp/sk101/prdf1.csv
 
+if [ $# -ne 1 ]
+then
+  echo I need 1 arg.
+  echo Demo:
+  echo $0 /tmp/sk101/prdf1.csv
+  exit 1
+fi
 
+echo Working...
